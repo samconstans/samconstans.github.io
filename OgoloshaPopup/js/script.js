1fc7ec7fc9f354f347d4ad354057311581c03d4a
Anastasiya Vovk <nastia-vovk@ukr.net>
2016-11-13 14:45:25 +0200

    added OgoloshaPopUp

---
+++ OgoloshaPopup/js/script.js
@@ -0,0 +1,22 @@
+
+
+$(function () {
+	//script for popups
+	$('a.show--popup').click(function () {
+		$('div.'+$(this).attr("rel")).fadeIn(500);
+		$("body").append("<div id='overlay'></div>");
+		$('#overlay').show().css({'filter' : 'alpha(opacity=80)'});
+		return false;				
+	});	
+	$('a.close').click(function () {
+		$(this).parent().fadeOut(100);
+		$('#overlay').remove('#overlay');
+		return false;
+	});
+	$('a.arrow__back').click(function () {
+		$(this).parent().fadeOut(100);
+		$('#overlay').remove('#overlay');
+		return false;
+	});
+	
+});	
